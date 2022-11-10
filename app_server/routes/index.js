8e348178cdba8898318414a221bb966a0d8e4cb9
melike1313 <melikenogay13@gmail.com>
2022-11-10 11:55:35 +0300

    mongoDB baglantısı yapıldı

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain=require('../controllers/main');
+var ctrlMekanlar=require('../controllers/mekanlar');
+var ctrlDigerleri=require('../controllers/digerleri');
 /* GET home page. */
-router.get('/',ctrlMain.index);
-module.exports = router;
+router.get('/', ctrlMekanlar.anaSayfa );
+router.get('/mekan', ctrlMekanlar.mekanBilgisi );
+router.get('/mekan/yorum/yeni', ctrlMekanlar.yorumEkle );
+router.get('/hakkinda', ctrlDigerleri.hakkinda );
+   
+
+module.exports = router
