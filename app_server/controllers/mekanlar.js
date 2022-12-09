f713a23fee8c302d9af8b1a8b1ce6cbeb26ba510
melike1313 <melikenogay13@gmail.com>
2022-12-09 13:17:24 +0300

    odev8 upload

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -91,7 +91,7 @@ const mekanBilgisi = function (req, res) {
   axios
     .get(apiSecenekleri.sunucu + apiSecenekleri.apiYolu + req.params.mekanid)
     .then((response) => {
-      req.session.mekanAdi=response.data.ad;
+      req.session.mekanAdi = response.data.ad;
       detaySayfasiOlustur(res, response.data);
     })
     .catch((hata) => {
@@ -100,146 +100,45 @@ const mekanBilgisi = function (req, res) {
 };
 
 const yorumEkle = function (req, res) {
-  var mekanAdi=req.session.mekanAdi;
-  var mekanid=req.params.mekanid;
-  if(!mekanAdi){
-    res.redirect("/mekan/"+mekanid);
-  }else{
-  res.render("yorumekle", { "baslik":mekanAdi+"mekanına yorum ekle",title: "Yorum Sayfası" });
-  }
+  var mekanAdi = req.session.mekanAdi;
+  var mekanid = req.params.mekanid;
+  if (!mekanAdi) {
+    res.redirect("/mekan/" + mekanid);
+  } else
+    res.render("yorumekle", {
+      baslik: mekanAdi + " mekanına yorum ekle",
+      title: "Yorum Sayfası",
+    });
 };
 
 const yorumumuEkle = function (req, res) {
-  var gonderilenYorum,mekanid;
-  mekanid=req.params.mekanid;
-  console.log("burdayim");
-  if(!req.body.adsoyad || !req.body.yorum){
-    res.redirect("/mekan/"+mekanid+"/yorum/yeni?hata=evet");
-  }
-  else{
-    gonderilenYorum={
-      yorumYapan:req.body.adsoyad,
-      puan:req.body.puan,
-      yorumMetni:req.body.yorum
-    }
-    console.log(gonderilenYorum);
-    axios.post(apiSecenekleri.sunucu+apiSecenekleri.apiYolu+mekanid+"/yorumlar",gonderilenYorum).then(function(){
-      res.redirect("/mekan/"+mekanid);
-    });
+  var gonderilenYorum, mekanid;
+  mekanid = req.params.mekanid;
+  if (!req.body.adsoyad || !req.body.yorum) {
+    res.redirect("/mekan/" + mekanid + "/yorum/yeni?hata=evet");
+  } else {
+    gonderilenYorum = {
+      yorumYapan: req.body.adsoyad,
+      puan: req.body.puan,
+      yorumMetni: req.body.yorum,
+    };
+    axios
+      .post(
+        apiSecenekleri.sunucu + apiSecenekleri.apiYolu + mekanid + "/yorumlar",
+        gonderilenYorum
+      )
+      .then(() => {
+        res.redirect("/mekan/" + mekanid);
+      })
+      .catch(() => {
+        console.log("hata");
+      });
   }
 };
+
 module.exports = {
   anaSayfa,
   mekanBilgisi,
   yorumEkle,
-  yorumumuEkle
+  yorumumuEkle,
 };
-
-// const axios = require("axios")
-
-// var apiSecenekleri = {
-//   sunucu: "http://localhost:3000",
-//   apiYolu: "/api/mekanlar/"
-// }
-
-// var mesafeyiFormatla = function (mesafe) {
-//   var yeniMesafe, birim
-//   if (mesafe > 1) {
-//     yeniMesafe = parseFloat(mesafe).toFixed(1)
-//     birim = " km"
-//   }
-//   else {
-//     yeniMesafe = parseInt(mesafe * 1000, 10)
-//     birim = " m"
-//   }
-//   return yeniMesafe + birim
-// }
-
-// const anaSayfaOlustur = function (res, mekanListesi) {
-//   var mesaj
-//   if (!(mekanListesi instanceof Array)) {
-//     mesaj = "API hatası!"
-//     mekanListesi = []
-//   }
-//   else {
-//     if (!mekanListesi.length) {
-//       mesaj = "Civarda herhangi bir mekan yok."
-//     }
-//   }
-//   res.render("anasayfa", {
-//     "baslik": "Anasayfa",
-//     "sayfaBaslik": {
-//       "siteAd": "Mekanbul",
-//       "slogan": "Mekanları Keşfet"
-//     },
-//     "mekanlar": mekanListesi,
-//     "mesaj": mesaj
-//   })
-// }
-
-// const anaSayfa = function (req, res, next) {
-//   axios.get(apiSecenekleri.sunucu + apiSecenekleri.apiYolu, {
-//     params: {
-//       enlem: req.query.enlem,
-//       boylam: req.query.boylam
-//     }
-//   }).then(function (response) {
-//     var i, mekanlar
-//     mekanlar = response.data
-//     for (i = 0; i < mekanlar.length; i++) {
-//       mekanlar[i].mesafe = mesafeyiFormatla(mekanlar[i].mesafe)
-//     }
-//     anaSayfaOlustur(res, mekanlar)
-//   }).catch(function (hata) {
-//     anaSayfaOlustur(res, hata)
-//   })
-// }
-
-// const mekanBilgisi = function (req, res, next) {
-//   res.render('mekanbilgisi', {
-//     'baslik': 'Mekan Bilgisi',
-//     'mekanBaslik': 'Starbucks',
-//     'mekanDetay': {
-//       'ad': 'Starbucks',
-//       'puan': 4,
-//       'adres': 'Centrum Garden AVM',
-//       'saatler': [
-//         {
-//           'gunler': 'Pazartesi-Cuma',
-//           'acilis': '07:00',
-//           'kapanis': '23:00',
-//           'kapali': false
-//         },
-//         {
-//           'gunler': 'Cumartesi-Pazar',
-//           'acilis': '09:00',
-//           'kapanis': '22:00',
-//           'kapali': false
-//         }
-//       ],
-//       'imkanlar': ['Dünya Kahveleri', 'Kek', 'Pasta'],
-//       'koordinatlar': {
-//         'enlem': 37.78,
-//         'boylam': 30.56
-//       },
-//       'yorumlar': [
-//         {
-//           'yorumYapan': 'Umut Selek',
-//           'yorumMetni': 'Mükemmel bir iş yeri. Güler yüzlü, tatlı dilli çalışanları var. Hijyenik bir yer.',
-//           'tarih': '8 Ağustos 2022',
-//           'puan': 4
-//         }
-//       ]
-//     }
-//   });
-// }
-
-// const yorumEkle = function (req, res, next) {
-//   res.render('yorumekle', { title: 'Yorum Ekle' });
-// }
-
-// module.exports = {
-//   anaSayfa,
-//   mekanBilgisi,
-//   yorumEkle
-// }
